d27af0959b759ce9271d0ae8df6af946c0735c31
xanxan <anna.emilia.salo@gmail.com>
2014-01-15 13:36:51 +0200

    Projektin aloitus
    Dokumentointi lisätty

---
+++ Runogeneraattori/src/main/java/superlabra/runogeneraattori/App.java
@@ -0,0 +1,13 @@
+package superlabra.runogeneraattori;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
